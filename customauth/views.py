e39d6e5bb21a1e6461b375cc72b498454cde5215
Abdullah Nafees <abdullah.nafees@edly.io>
2022-10-06 16:41:07 +0500

    Feat/basic auth

---
+++ customauth/views.py
@@ -0,0 +1,35 @@
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.response import Response
+
+from .models import CustomUser
+from .serializers import CustomUserSerializer, CustomUserLoginSerializer
+
+
+class ListUsers(ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class RegisterUser(CreateAPIView):
+    serializer_class = CustomUserSerializer
+
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = CustomUserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["username"]
+        token, created = Token.objects.get(user=user.pk)
+        return Response(
+            {"token": token.key, "username": user.username, "email": user.email},
+            status=status.HTTP_200_OK,
+        )
